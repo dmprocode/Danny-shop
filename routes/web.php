a5bdab91476bfc91441ca4352e8744c69ae85287
dmprocode <danielmathias2022@gmail.com>
2025-02-26 12:38:10 +0300

    separe user based on theire role

--- routes/web.php
+++ routes/web.php
@@ -22,19 +22,30 @@ Route::get('/',[UserController::class, 'loginIndex'])->name('login');
 Route::post('/login',[UserController::class,'loginForm'])->name('login-data');
 // =================End of login===================================
 
-Route::get('/admin/dashboard', [UserController::class, 'adminDashboard'])->name('admin-dashboard');
-Route::get('/admin/users', [UserController::class, 'userTable'])->name('user-index');
-Route::post('/admin/users', [UserController::class, 'addUsers'])->name('add-user');
-Route::post('/admin/user-update', [UserController::class, 'updateUser'])->name('update-user');
 
 
 
-// ==============Shopkeeper ==================
-Route::get('/shopkpeeper/dashboard',[ShopkeeperController::class, 'dashboardIndex'])->name('shopkeeper-dashboard');
+
 
 //===========Unauthorized==================
 Route::get('/unathorized',[LoginController::class,'unauthorized'])->name('unathorized');
 
+
+Route::middleware(['auth', 'role:admin'])->group(function () {
+    Route::get('/admin/dashboard', [UserController::class, 'adminDashboard'])->name('admin-dashboard');
+    Route::get('/admin/users', [UserController::class, 'userTable'])->name('user-index');
+    Route::post('/admin/users', [UserController::class, 'addUsers'])->name('add-user');
+    Route::post('/admin/user-update', [UserController::class, 'updateUser'])->name('update-user');
+});
+
+Route::middleware(['auth', 'role:shopkeeper'])->group(function () {
+// ==============Shopkeeper ==================
+Route::get('/shopkpeeper/dashboard',[ShopkeeperController::class, 'dashboardIndex'])->name('shopkeeper-dashboard');
+});
+
+Route::middleware(['auth', 'role:customer'])->group(function () {
+    Route::get('/customer/dashboard', [CustomerController::class, 'index']);
+});
 
 
 
