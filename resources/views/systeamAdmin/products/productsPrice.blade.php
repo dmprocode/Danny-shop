3002380249abb350b42ab4585649e13b652a80ba
dmprocode <danielmathias2022@gmail.com>
2025-03-31 15:19:27 +0300

    Fixing bags

--- resources/views/systeamAdmin/products/productsPrice.blade.php
+++ resources/views/systeamAdmin/products/productsPrice.blade.php
@@ -71,7 +71,7 @@
                                         <tr>
                                             <td>{{$key + 1}}</td>
                                             <td>{{$price->name}} </td>
-                                            <td>{{$price->buying_price}}</td>
+                                            <td>{{number_format($price->buying_price,2)}}</td>
                                             @if($price->number_pieces == '')
                                             <td class="text-danger fw-bold">No Value</td>
                                             @else
@@ -94,7 +94,13 @@
                                             <td>{{number_format($price->selling_price_per_piece),2}}</td>
                                             @endif
                                             <td>
-                                                <a href="javascript:void(0);" class="action-icon"> <i class="mdi mdi-square-edit-outline"></i></a>
+                                                <a href="javascript:void(0);" class="action-icon"> <i class="mdi mdi-square-edit-outline edit-product-price"
+                                                data-id= "{{$price->id}}",
+                                                data-selling_price_per_dozen= "{{$price->selling_price_per_dozen}}",
+                                                data-selling_price_per_piece= "{{$price->selling_price_per_piece}}",
+                                                data-name= "{{$price->name}}",
+                                                data-buying_price = "{{$price->buying_price}}"
+                                                ></i></a>
                                             </td>
                                         </tr>
                                         @endforeach
@@ -114,6 +120,84 @@
         </div>
     </div>
 
+
+    <div class="row" id="up-product-price-div">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-body">
+                    <div class="row mb-2">
+                        <div class="col-sm-5">
+                            <a href="javascript:void(0);"
+                                class="btn btn-outline-danger btn-sm text-info mb-2 go-back-btn"><i
+                                    class="mdi mdi-plus-circle me-2 h4"></i> <span class="h5">Go Back </span> </a>
+                        </div>
+                        <div class="col-sm-7 ">
+                            <div class="text-sm-end">
+                                <a href="{{ route('admin-dashboard')}}" class="btn btn- mb-2 me-1  text-light root-btn"
+                                    style="background-color:rgb(24,4,24)"><i class="mdi mdi-arrow-left font-16"> Go
+                                        Back</i></a>
+
+                            </div>
+                        </div><!-- end col-->
+
+                        <form action="#" id="up-productsFormData">
+                            @csrf
+                            <input type="hidden" id="up_id">
+                            <div class="row">
+                               <input type="hidden" id="id" name="id">
+                                <div class="mb-3 col-md-6">
+                                    <label for="simpleinput" class="form-label fw-normal fs-16 ">Product Name:</label>
+                                    <input type="text" id="up_productname" class="form-control form-control-sm "
+                                        name="up_productname">
+                                    <i class="text-danger up_productname message fw-normal role font-16"></i>
+
+                                </div>
+
+                                <div class="mb-3 col-md-6">
+                                    <label for="simpleinput" class="form-label fw-normal fs-16 ">Buying Price:</label>
+                                    <input type="text" id="up_buying_price" class="form-control form-control-sm "
+                                        name="up_buying_price">
+                                    <i class="text-danger up-buying_price message fw-normal role font-16"></i>
+
+                                </div>
+
+
+                                <div class="mb-3 col-md-6">
+                                    <label for="simpleinput" class="form-label fw-normal fs-16 ">Selling Price per Piece:</label>
+                                    <input type="text" id="up_price_perIteams" class="form-control form-control-sm "
+                                        name="up_price_perIteams">
+                                    <i class="text-danger up-buying_price message fw-normal role font-16"></i>
+
+                                </div>
+
+                                <div class="mb-3 col-md-6">
+                                    <label for="simpleinput" class="form-label fw-normal fs-16 ">Selling Price per Dazeen:</label>
+                                    <input type="text" id="up_price_perIteams" class="form-control form-control-sm "
+                                        name="up_price_perIteams">
+                                    <i class="text-danger up-buying_price message fw-normal role font-16"></i>
+
+                                </div>
+
+
+                            </div>
+                            <center>
+                                <div class="d-flex justify-content-center">
+                                    <button class="btn btn- h4 fw-bold text-light  w-50 " id=""
+                                        style="background-color:teal">Update Product  Price <span
+                                            class="uil-plus"></span></button>
+                                </div>
+
+                            </center>
+
+                        </form>
+
+
+
+                    </div> <!-- end card-body-->
+                </div> <!-- end card-->
+            </div> <!-- end col -->
+        </div>
+    </div>
 
 
 
